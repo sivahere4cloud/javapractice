6ef18630866505ca6ed738498147b0a4b1f14146
sivahere4cloud <141396181+sivahere4cloud@users.noreply.github.com>
2023-10-05 02:02:49 +0100

    String and String builder practice

---
+++ src/com/siva/string/stringbasic.java
@@ -0,0 +1,34 @@
+package com.siva.string;
+
+public class stringbasic {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        String str=new String();
+        System.out.println(str.hashCode());
+        String name="Siva Ganesh";
+        System.out.println(name+str.concat(" here"));
+
+        String s1="Siva Ganesh";
+        System.out.println(s1.hashCode());
+        System.out.println(s1.concat(" here").hashCode());
+        String s2=" Ganesh";
+        System.out.println(s2.hashCode());
+
+        System.out.println("String Buffer");
+        //StringBuffer
+        StringBuffer sb=new StringBuffer();
+        System.out.println(sb.length());
+        System.out.println(sb.capacity());
+        sb.append("Siva Ganesh");
+        System.out.println(sb.hashCode());
+        System.out.println(sb.append(" here"));
+        System.out.println(sb.hashCode());
+        sb.append("Siva Ganesh");
+        System.out.println(sb.hashCode());
+
+
+
+
+    }
+}
